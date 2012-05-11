c7b7d1f7f334b08fbcd228a2f4cb4f7364881688
dbh937 <dbh937@gmail.com>
2012-05-11 16:58:30 -0400

    Some changes

--- main.py
+++ main.py
@@ -17,17 +17,6 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
 
 
-def hash_str(s):
-  return hashlib.md5(s).hexdigest()
-
-def make_secure_val(s):
-  return "%s,%s" % (s, hash_str(s))
-
-def check_secure_val(h):
-  val = h.split(',')[0]
-  if h == make_secure_val(val):
-    return val
-
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
