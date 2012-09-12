a85ae6f5426c2a3b203a95049813fa6b398f830e
Davis Haupt <dbh937@gmail.com>
2012-09-11 21:47:17 -0400

    Fixed bug that on logout current page wouldn't save

--- assets/js/planner-buttons.js
+++ assets/js/planner-buttons.js
@@ -81,5 +81,24 @@ $(document).ready(function() {
       
     });
   });
+  
+  $("#logout").click(function() {
+  	$.ajax({
+      dataType: "json",
+      data: {'mondayeng': $("textarea#mondayeng").val(), 'tuesdayeng': $("textarea#tuesdayeng").val(), 'wednesdayeng': $("textarea#wednesdayeng").val(), 'thursdayeng': $("textarea#thursdayeng").val(), 'fridayeng': $("textarea#fridayeng").val(), 
+            'mondayhist': $("textarea#mondayhist").val(), 'tuesdayhist': $("textarea#tuesdayhist").val(), 'wednesdayhist': $("textarea#wednesdayhist").val(), 'thursdayhist': $("textarea#thursdayhist").val(), 'fridayhist': $("textarea#fridayhist").val(), 
+            'mondaymath': $("textarea#mondaymath").val(), 'tuesdaymath': $("textarea#tuesdaymath").val(), 'wednesdaymath': $("textarea#wednesdaymath").val(), 'thursdaymath': $("textarea#thursdaymath").val(), 'fridaymath': $("textarea#fridaymath").val(), 
+            'mondaysci': $("textarea#mondaysci").val(), 'tuesdaysci': $("textarea#tuesdaysci").val(), 'wednesdaysci': $("textarea#wednesdaysci").val(), 'thursdaysci': $("textarea#thursdaysci").val(), 'fridaysci': $("textarea#fridaysci").val(), 
+            'mondaylang': $("textarea#mondaylang").val(), 'tuesdaylang': $("textarea#tuesdaylang").val(), 'wednesdaylang': $("textarea#wednesdaylang").val(), 'thursdaylang': $("textarea#thursdaylang").val(), 'fridaylang': $("textarea#fridaylang").val(), 
+            'mondayother': $("textarea#mondayother").val(), 'tuesdayother': $("textarea#tuesdayother").val(), 'wednesdayother': $("textarea#wednesdayother").val(), 'thursdayother': $("textarea#thursdayother").val(), 'fridayother': $("textarea#fridayother").val(), 
+            'notes': $("textarea#notes").val(),
+            'action': "next", 'date': $("#date").text()
+      },
+      type: "POST",
+      success: function(data) {            
+		window.location.href = "/logout";
+      } 
+    });
+  });
 });
         
