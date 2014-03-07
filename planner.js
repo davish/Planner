7f383b4eec5886b808f42ce2cdfd3de6d1c56db7
Davis Haupt <dbh937@gmail.com>
2014-03-06 21:07:54 -0500

    Sidebar now shows the assignments for the next schoolday, in a nice way with unordered lists.

--- planner.js
+++ planner.js
@@ -13,21 +13,6 @@ var colors = {
 
               }
 
-              /* 
-
-                "A": "#FF0000",
-                "B": "#FF7400",
-                "C": "#1826B0",
-                "D": "#086CA2",
-                "E": "#00C12B",
-                "F": "#F0FC00",
-                "G": "#B1009B",
-                "H": "#580EAD",
-                "I": "#FFC700",
-                "J": "1240AB",
-                "K": "#BADA55"
-              */
-
 var schedule = {
                     "1": {
                         "1": "B",
@@ -80,18 +65,109 @@ var schedule = {
                         "8": "H"
                     }
                 }
+var boxClicked = "";
 
 function main() {
-    // console.log($("div"));
+    $(".sidebar").html("<h3>" + getDayName(getNextSchoolDay(new Date())) + "</h3>");    
+
     $(".period").each(function(index, value) {
         var id = value.id.split('');
-        var prd = colors[schedule[id[0]][id[1]]];
         var identifier = "#" + value.id;
-
-        $(identifier).html('<div class="letter">' + schedule[id[0]][id[1]] + "</div>");
+        $(identifier).children(".letter").html(schedule[id[0]][id[1]]);
+        $(identifier).children(".close").hide();
+        $(identifier).children("textarea").hide();
         $(identifier).css("background-color", colors[schedule[id[0]][id[1]]]);
     });
-    $(".period").click(function(e) {
+
+    $(".letter").click(function(e) {
         periodID = $(this).attr("id");
-    })
+        if (boxClicked == "") {
+            boxClicked = periodID;
+
+            $(this).parent().animate({"width": 200, "height": 200});
+            $(this).slideUp();
+            $(this).parent().children(".close").slideDown();
+            $(this).parent().children("textarea").slideDown();
+            $(this).parent().children("textarea").focus();
+        }  
+
+    });
+
+    $(".close").children("a").click(function() {
+        if (boxClicked == periodID) {
+            boxClicked = "";
+
+            $(this).parent().slideUp();
+            $(this).parent().parent().animate({"width": 150, "height": 70});
+            $(this).parent().parent().children("textarea").slideUp();
+            $(this).parent().parent().children(".letter").slideDown();
+
+            checkForHW(getNextSchoolDay(new Date()));
+        }
+    });
+}
+
+function checkForHW(day) {
+    $(".sidebar").html("<h3>" + getDayName(day) + "</h3> <hr>");
+    for (var i = 1; i <= 8; i++ ) {
+        var cellID = String(day) + String(i)
+        var TAval = $("#" + cellID).children("textarea").val();
+        if (TAval != ""){
+            var assignments = TAval.split('\n');
+            var toDo = "";
+            for (var j = 0; j < assignments.length; j++) {
+                if (assignments[j] != "") {
+                    toDo = toDo + "<li>" + assignments[j] + "</li>";
+                }
+            }
+
+            $(".sidebar").html($(".sidebar").html() + "<b>" + getClass(cellID) + " Period:</b> <ul>" + toDo + "</ul> <hr>");
+        }
+    }
+}
+
+function getClass(s) {
+    var class_ID = s.split('');
+    return schedule[class_ID[0]][class_ID[1]];
+}
+
+function getNextSchoolDay(date) {
+    console.log(date.getDay());
+    var day = 0;
+    if (date.getDay() > 0 && date.getDay() < 5)  // getDate() returns int 0-6 (sunday-saturday)
+        day = date.getDay() + 1;
+    else 
+        day = 1;
+    return day;
+}
+
+function getDayName(d) {
+    var s = ""
+    switch(d) {
+        case 0:
+            s = "Sunday";
+            break;
+        case 1:
+            s = "Monday";
+            break;
+        case 2:
+            s = "Tuesday";
+            break;
+        case 3:
+            s = "Wednesday";
+            break;
+        case 4:
+            s = "Thursday";
+            break;
+        case 5:
+            s = "Friday";
+            break;
+        case 6:
+            s = "Saturday";
+            break;
+        default:
+            s = "You Suck";
+            break;
+    }
+    return s;
 }
