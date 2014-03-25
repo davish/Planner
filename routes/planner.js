a4a23420b5c2ea2f6c505a9f0727dd75a9efb12e
Davis Haupt <dbh937@gmail.com>
2014-03-24 22:53:48 -0400

    started implementing update functions for the different weeks.

--- routes/planner.js
+++ routes/planner.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose'),
     schemas = require('./schemas');
 var Schema = mongoose.Schema;
 
-// test user username is davish.
+// test username is davish.
 
 exports.get = function(req, res) {
   res.render("index");
@@ -11,50 +11,28 @@ exports.get = function(req, res) {
 exports.post = function(req, res) {
     var ref = {}
     var reqM = new Date(req.body.monday);
-    mongoose.connect('mongodb://localhost/test');
-    var db = mongoose.connection;
-    
+
+    console.log(req.body);
+
     if (req.body.req == "sync") {
-        db.on('open', function() {
-            getSettings(ref, function() {
-                getWeek(getMonday(new Date(req.body.monday)), ref, function() {
-                    db.close(function() {
-                        res.json(ref);
-                    });
-                });
+        getSettings("davish", ref, function() {
+            getWeek("davish", getMonday(new Date(req.body.monday)), {}, ref, function() {
+                res.json(ref);
             });
         });
-    } else if (req.body.req == "prev") {
-        db.on('open', function() {
-            ref.colorCode = req.body.colorCode;
-            ref.colors = req.body.colors;
-            ref.keywords = req.body.keywords;
-            ref.kwStyle = req.body.kwStyle;
-            ref.schedule = req.body.schedule;
-            getWeek(new Date(reqM.getFullYear(), reqM.getMonth(), reqM.getDate() - 7), ref, function() {
-                db.close(function) {
-                    res.json(ref);
-                }
-            });
-        });
-    } else if (req.body.req == "next") {
-        db.on('open', function() {
-            ref.colorCode = req.body.colorCode;
-            ref.colors = req.body.colors;
-            ref.keywords = req.body.keywords;
-            ref.kwStyle = req.body.kwStyle;
-            ref.schedule = req.body.schedule;
-            getWeek(new Date(reqM.getFullYear(), reqM.getMonth(), reqM.getDate() - 7), ref, function() {
-                db.close(function) {
-                    res.json(ref);
-                }
-            });
-        });   
     }
-
-
-    // if (req.body.req = "sync")
-    //     res.json(ref);
+    else if (req.body.req == "prev") {
+        getWeek("davish", new Date(reqM.getFullYear(), reqM.getMonth(), reqM.getDate() - 7), req.body.assignments, ref, function() {
+            res.json(ref);
+        });
+    }
+    else if (req.body.req == "next") {
+        getWeek(new Date(reqM.getFullYear(), reqM.getMonth(), reqM.getDate() + 7), req.body.assignments, ref, function() {
+            res.json(ref);
+        });
+    } else {
+        res.json(500, {"error": "proper 'req' parameter not provided."});
+    }
 }
 
 
@@ -64,7 +42,7 @@ var Schedule = mongoose.model('schedule', schemas.scheduleSchema);
 var currentEvent = mongoose.model('currentEvents', schemas.currentEventsSchema);
 
 
-function getSettings(obj, c) {
+function getSettings(usr, obj, c) {
     Setting.findOne({"name": "colorCode"}, function(err, colorCode) {
         if (!err)
             obj.colorCode = JSON.parse(colorCode.data);
@@ -74,37 +52,74 @@ function getSettings(obj, c) {
             Setting.findOne({"name": "keywords", "user": "davish"}, function(err, keywords) {
                 if (!err)
                     obj.keywords = keywords.data;
-
-                Setting.findOne({"name": "kwStyle", "user": "davish"}, function(err, kwStyle) {
+                Setting.findOne({"name": "kwStyle", "user": usr}, function(err, kwStyle) {
                     if (!err)
                         obj.kwStyle = JSON.parse(kwStyle.data);
-                    Schedule.findOne({"user": "davish", "year": new Date(2014, 0)}, function(err, schedule) {
+                    Schedule.findOne({"user": usr, "year": new Date(2014, 0)}, function(err, schedule) {
                         if (!err)
                             obj.schedule = JSON.parse(schedule.data);
-                        c && c();
+                        if (c)
+                            c();
                     });
                 });
             });
         });
     });
-
 }
 
 
-function getWeek(d, obj, c) {
-    Week.findOne({"user": "davish", "monday": getMonday(new Date(d))}, function(err, week) {
-        if (!err) {
-            obj.monday = week.monday;
-            obj.assignments = JSON.parse(week.data);
-        } 
+function getWeek(usr, d, asst, obj, c) {
+    Week.findOne({"user": usr, "monday": getMonday(new Date(d))}, function(err, week) {
+        if (!week) {
+            week = new Week({
+                user: usr,
+                monday: getMonday(new Date(d)),
+                data: JSON.stringify(asst)
+            });
+            week.save(function() {
+                obj.monday = week.monday;
+                obj.assignments = JSON.parse(week.data);
+            });
+        } else {
+            if (!err) {
+                console.log(week);
+                if (asst)
+                    week.assignments = JSON.stringify(asst);
+                week.save(function() {
+                    obj.monday = week.monday;
+                    obj.assignments = JSON.parse(week.data);
+                });
+            }
+        }
         currentEvent.findOne({"monday": obj.monday}, function(err, events) {
-            if(!err)
-                obj.currentEvents = JSON.parse(events.data);
-            c && c();
+            if (!events) {
+                events = new currentEvent({
+                    monday: obj.monday,
+                    data: JSON.stringify([])
+                });
+                events.save(function() {
+                    obj.currentEvents = JSON.parse(events.data);
+                });
+            } else {
+                if(!err)
+                    obj.currentEvents = JSON.parse(events.data);
+            }
+            if (c)
+                c();
         });
     });
 }
 
 function getMonday(d) {
   return new Date(d.getFullYear(), d.getMonth(), d.getDate() - (d.getDay() - 1));
+}
+
+function genAsst() {
+    var obj = {};
+    for (var i = 1; i <= 5; i++) {
+        for (var j = 1; j <=9; j++) {
+            obj[String(i)+String(j)] = "";
+        }
+    }
+    return obj;
 }
